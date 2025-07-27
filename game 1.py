3a39222bc4be89a848d9d29aa6c66df628688caf
kelenksq <kola.viktorov11@gmail.com>
2025-07-27 14:26:47 +0300

    GAME

---
+++ game 1.py
@@ -0,0 +1,8 @@
+import random
+x = random.randint(-1, 1)
+print("Value of x:", x)
+
+if x > 0:
+    print("x is positive")
+else:
+    print("x is negative or zero")
---
+++ game 2.py
@@ -0,0 +1,8 @@
+answer = input("Are you stupid? (yes/no)\n")
+
+if answer == "yes":
+    print("Ha-ha, you are stupid!")
+elif answer == "no":
+    print("Respect, you are not stupid!")
+else:
+    print("Please answer 'yes' or 'no'.")
